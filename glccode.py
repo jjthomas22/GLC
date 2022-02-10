301d20aa7c1569d33234662ea0a2d06137b5932f
Chuks_Nwakanma <52938232+ChuksKN@users.noreply.github.com>
2022-02-10 12:44:57 -0700

    Update glccode.py
    Starting comments and documentation

--- glccode.py
+++ glccode.py
@@ -5,7 +5,9 @@ Created on Wed Jan  5 14:41:30 2022
 @author: JJTHOMAS
 """
 
+# Note: To add documentation "Notes" for each significant code block. To improve readability and code functionality understanding
 
+# Note: ...
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
@@ -14,30 +16,36 @@ import os, uuid
 import numpy as np
 import pyodbc 
 import logging
-#logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',level=logging.INFO,datefmt='%Y-%m-%d %H:%M:%S')
 import warnings
-warnings.filterwarnings("ignore")
 import configparser
 
+warnings.filterwarnings("ignore")
+#logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',level=logging.INFO,datefmt='%Y-%m-%d %H:%M:%S')
+
+# Note: ...
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s', 
                               '%m-%d-%Y %H:%M:%S')
 
+# Note: ...
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.DEBUG)
 stdout_handler.setFormatter(formatter)
 
+# Note: ...
 file_handler = logging.FileHandler('intermediate.log')
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
+# Note: ...
 logger.addHandler(file_handler)
 logger.addHandler(stdout_handler)
 
 
-
+# Note: ...
 try:
+    # Note: ...
     logging.info("Reading credentials file")
     config = configparser.ConfigParser()
     config.read('credentials.ini')
@@ -50,16 +58,15 @@ try:
 
     cursor = conn.cursor()
     logging.info("connected to database")
-    
-    
-
 
     url = 'https://wbea.org/asi-php/ams_hist/helpers/hist_table.php?s=2&f=T&sy=2021&sm=7&sd=8&sh=0&ey=2021&em=7&ed=9&eh=23&tt=l1mn'
 
-
+    # Note: ...
     toDateYY = datetime.today().strftime('%Y')
     toDateMM = datetime.today().strftime('%m')
     toDateDD = datetime.today().strftime('%d')
+
+    # Note: ...
     logging.info("running upto year {}-{}-{}".format(toDateDD,toDateMM,toDateYY))
 
     fromDate = datetime.today() - timedelta(days=1)
@@ -67,19 +74,25 @@ try:
     fromDateMM = fromDate.strftime('%m')
     fromDateDD = fromDate.strftime('%d')
 
+    # Note: ...
     logging.info("running from {}-{}-{}".format(fromDateDD,fromDateMM,fromDateYY))
+
+    # Note: ...
     stations = ['2','4','5','11']
     all_available_dfs=[]
+
+    # Note: ...
     for station in stations:
         logging.info("running for station {}".format(station))
 
         endpoint = 'https://wbea.org/asi-php/ams_hist/helpers/hist_table.php?s='+ station +'&f=T&sy=' + fromDateYY + '&sm=' + fromDateMM + '&sd=' + fromDateDD + '&sh=0&ey=' + toDateYY + '&em=' + toDateMM + '&ed=' + toDateDD + '&eh=23&tt=l1mn'
 
-
-
+        # Note: ...
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
         r = requests.get(endpoint, headers=headers) 
         logging.info("api response status code {}".format(r.status_code))
+
+        # Note: ...
         if r.status_code == 200:
             if station=='2':
         # may require bs4, lxml, html5lib in environment
@@ -116,21 +129,34 @@ try:
                 #logging.info(" no.of colums are {}".format(df_11.shape[1]))
         else:
             logging.error(f'Error. Status code is {r.status_code}')
+    
+    # Note: ...
     all_dfs=pd.concat(all_available_dfs, axis=0)
+
+    # Note: ...
     logging.info("no.of data points for merged data is {}".format(all_dfs.shape[0]))
     #logging.info(" no.of colums of merged data is {}".format(all_dfs.shape[1]))
+
+    # Note: ...
     columns_list=all_dfs.columns.tolist()
     columns_list.append(columns_list.pop(columns_list.index('station_id')))
+
     final_df=all_dfs[columns_list].copy()
     final_df=final_df.astype('str')
+
     params = [tuple(x) for x in final_df.values]
+
     columns_list = final_df.columns.tolist()
     columns_list_query = f'({(",".join(columns_list))})'
+
     sr_columns_list = [f'Source.{i}' for i in columns_list]
     sr_columns_list_query = f'({(",".join(sr_columns_list))})'
+
     rows_to_insert = [row.tolist() for idx, row in final_df.iterrows()]
     rows_to_insert = str(rows_to_insert).replace('[', '(').replace(']', ')')[1:][:-1]
     table_name='dbo.GenericTable'
+
+    # Note: ...
     query = f"MERGE INTO {table_name} as Target \
             USING (SELECT * FROM \
             (VALUES {rows_to_insert}) \
@@ -139,10 +165,16 @@ try:
             ON Target.station_id=Source.station_id AND Target.Date_Time=Source.Date_Time \
             WHEN NOT MATCHED THEN \
             INSERT {columns_list_query} VALUES {sr_columns_list_query};"
+    
+    # Note: ...
     logging.info("inserting {} records into database".format((all_dfs.shape[0])))
+
     cursor.execute(query)
     conn.commit()
-    logging.info("inserted into the database")   
+
+    # Note: ...
+    logging.info("inserted into the database")
+
 except Exception as ex:
     logging.error('Exception is : {}'.format(ex))
 
